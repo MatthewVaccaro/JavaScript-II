cbaefc053a89dfaaec905bba23cea2e5c8814e64
Matthew Vaccaro <matthew.vaccaro@Matthews-MacBook-Pro.local>
2019-09-05 20:10:40 -0500

    Project Complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -28,6 +28,8 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   const test1 = firstItem(items, item => `I love my ${item}!`);
   console.log(test1); // "I love my Pencil!"
 
+  
+
   // TEST 2 (declaring callback before hand):
 
   function logExorbitantPrice(article) {
@@ -38,27 +40,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
+getLength(items, length => {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, lastItem => {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
-function multiplyNums(x, y, cb) {
+sumNums(69, 69, addition => {
+  console.log(addition);
+});
+
+function multiplyNums(x, y, mulitplyThemBoiz) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return mulitplyThemBoiz(x * y);
 }
+
+multiplyNums(69, 69, mulitplyThemBoiz => {
+  console.log(mulitplyThemBoiz);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+contains("pants", items, bool => {
+  console.log(bool);
+});
 
 /* STRETCH PROBLEM */
 
